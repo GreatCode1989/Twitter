d994c36667dff83b70b8d59ff0cb0e79f473d5ad
GeateCode <wef666wef6@gmail.com>
2023-09-18 20:06:18 +0300

    lo

---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="h-screen flex flex-col">
+    <div
+      class="p-2 my-2 hover:bg-blue-50 rounded-full w-min"
+      :class="defaultTransition"
+    >
+      <nuxt-link to="/">
+        <div class="w-8 h-8"><LogoTwitter /></div
+      ></nuxt-link>
+    </div>
+    <SidebarLeftTab active>
+      <template v-slot:icon>
+        <HomeIcon />
+      </template>
+      <template v-slot:name> Home </template>
+    </SidebarLeftTab>
+  </div>
+</template>
+
+<script setup>
+import { HomeIcon } from "@heroicons/vue/20/solid";
+
+const { defaultTransition } = useTailwindConfig();
+</script>
